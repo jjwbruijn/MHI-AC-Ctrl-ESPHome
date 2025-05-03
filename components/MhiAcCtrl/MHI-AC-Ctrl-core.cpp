47afe6221e4e388d246d092f5406216579599f20
Jelmer <jelmer_bruijn@hotmail.com>
2025-05-04 00:16:11 +0200

    Update MHI-AC-Ctrl-core.cpp

--- components/MhiAcCtrl/MHI-AC-Ctrl-core.cpp
+++ components/MhiAcCtrl/MHI-AC-Ctrl-core.cpp
@@ -267,7 +267,7 @@ int MHI_AC_Ctrl_Core::loop(uint max_time_ms) {
         if ((millis() - SCKMillis > 2)&&(byte_cnt))
           return byte_cnt;
       } 
-      delayMicroseconds(100);
+      delayMicroseconds(30);
       // bit out
       if ((MISO_frame[byte_cnt] & bit_mask) > 0)
         digitalWrite(MISO_PIN, 1);
@@ -281,7 +281,7 @@ int MHI_AC_Ctrl_Core::loop(uint max_time_ms) {
       if (digitalRead(MOSI_PIN))
         MOSI_byte += bit_mask;
       bit_mask = bit_mask << 1;
-      delayMicroseconds(100);
+      delayMicroseconds(30);
     }
     if ((((MOSI_frame[SB0] & 0xfe) != 0x6c) | (MOSI_frame[SB1] != 0x80) | (MOSI_frame[SB2] != 0x04))&&(byte_cnt == 4))
       return -13;
